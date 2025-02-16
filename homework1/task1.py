8ffa7f32e3dd3ee701c7836a786367a02fae8eaf
Ronan Hella <rhella@uccs.edu>
2025-02-16 04:12:15 +0000

    Task 1 Completed

--- homework1/task1.py
+++ homework1/task1.py
@@ -1,1 +1,10 @@
-"Homework 1 Task 1"
+"""Homework One Task One"""
+import pytest
+def main():
+    return "Hello World!"
+
+def test_main():
+    assert main() == "Hello World!"
+
+if __name__ == "__main__":
+    print(main())
