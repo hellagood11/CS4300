b089bc5c028ee8641f99bb13ed923f412605f3b3
Ronan Hella <rhella@uccs.edu>
2025-03-03 01:56:47 +0000

    updated error in serializer file (had MovieSerializer instead of model)

--- homework2/movie_theater_booking/bookings/serializers.py
+++ homework2/movie_theater_booking/bookings/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Movie, Seat, Booking
 
 #create serializer for movie with meta data of movie and all relevant fields
-class MovieSerializer(serializers.MovieSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
 #create seat serializer with meta data for seat and relevant class fields
-class SeatSerializer(serializers.SeatSerializer):
+class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seat
         fields = '__all__'
 
 #create serializer for booking with metadata for booking and relevant class fields
-class BookingSerializer(serializers.BookingSerializer):
+class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
