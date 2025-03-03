993f59eb958e6e6ce52dc138350680fa1f20cd84
Ronan Hella <rhella@uccs.edu>
2025-03-03 01:53:19 +0000

    Added the views python file

--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
+# create movies view
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+#create seat view
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+#create booking view but only with user authentication
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
