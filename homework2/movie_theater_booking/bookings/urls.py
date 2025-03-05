377b575e45306da3b690421866b6f5411637a857
Ronan Hella <rhella@uccs.edu>
2025-03-05 06:13:31 +0000

    Modified urls and added booking history path

--- homework2/movie_theater_booking/bookings/urls.py
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MovieViewSet, SeatViewSet, BookingViewSet, movie_list, seat_booking
-
+from . import views
+# api router paths
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
 router.register(r'seats', SeatViewSet)
 router.register(r'bookings', BookingViewSet)
-
+#url paths for each part of the website
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('list_movies/', movie_list, name='movie_list'), #add a movie list view 
+    path('movies/', views.movie_list, name='movie_list'), #add a movie list view 
     path('movies/<int:movie_id>/seats/',  seat_booking, name='seat_booking'),
+    path('booking_history/', views.booking_history, name='booking_history')
 ]
